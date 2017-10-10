087d0ee8056f884e58253e7c02cf6f6c1520a36c
Holst <moviemastersdk@gmail.com>
2017-10-10 19:03:56 +0200

    tiny-router done-ish

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const http = require('http')
 let TinyRouter = require('./tiny-router.js');
+var http = require('http');
+var fs = require('fs');
 
 var app = new TinyRouter();
 
-
 app.get('/home', function(request){
-    console.log("home requested!");
+    return "HOMEPAGE";
 });
 
-app.printRoutes();
+app.get('/lol', function(request){
+    return showPage('./pages/home.html');
+});
+
+
+function showPage(path){
+    return fs.readFileSync(path);
+    return result;
+}
+
+
 
 app.listen(8080);
