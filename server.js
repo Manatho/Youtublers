d9ee8930c71a5a938de305fe8bb163fc6d4ba688
Unknown <mathies-h@hotmail.com>
2017-11-07 01:02:21 +0100

    Added initial most viewed layout
    Actual data still missing

--- server.js
+++ server.js
@@ -6,23 +6,22 @@ var fs = require('fs');
 
 var app = new TinyRouter();
 
-app.get('/home', function(request){
+app.get('/home', function (request) {
     return showPage('./pages/home.html', {
         'Fisk': 'FUCKING OP',
-        people: [
-            {name: 'John Doe', age: Math.floor(Math.random() * 60), hobbies: ['climbing', 'skiing'] },
-            {name: 'Jack Sparrow', age: Math.floor(Math.random() * 60), hobbies: ['pirating', 'rum'] },
-            {name: 'Bond, James Bond', age: Math.floor(Math.random() * 60), hobbies: ['martini', 'shaken'] },
-            {name: 'Rick Sanchez', age: Math.floor(Math.random() * 60), hobbies: ['himself', 'not life'] },
-        ]
+        videos: [
+            { user: 'John Doe', name: 'Magical Joe', views: Math.floor(Math.random() * 200), uploaded: new Date(2012,0,1) },
+            { user: 'Jack Sparrow', name: 'Why is the rum gone', views: Math.floor(Math.random() * 200), uploaded: new Date(2012,0,1) },
+            { user: 'Bond, James Bond', name: 'Best theme song', views: Math.floor(Math.random() * 200), uploaded: new Date(2012,0,1) },
+            { user: 'Rick Sanchez', name: 'Adventure time', views: Math.floor(Math.random() * 200), uploaded: new Date(2012,0,1) },]
     });
 });
 
-function showPage(path, vars){
+function showPage(path, vars) {
     var file = fs.readFileSync(path);
     file = file.toString();
     file = HtmlPreprocessor.process(file, vars);
-    return file; 
+    return file;
 
 }
 
